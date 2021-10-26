7a00e0fc9496740d3c0a8bba33c180e513fdc169
yann.dufaure <yann.dufaure@gmail.com>
2021-10-26 17:03:04 +0200

    add an application to generate Simpson's caracter with an API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import react from 'react'
 import './App.css';
+import axios from 'axios';
+import QuoteCard from './components/QuoteCard';
+
 
 function App() {
+const [quotes, setQuotes] = react.useState({"quote":"I can't even say the word 'titmouse' without gigggling like a schoolgirl.","character":"Homer Simpson","image":"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939","characterDirection":"Right"})
+
+    const getQuote = () => {
+      axios
+        .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+        .then((res) => res.data)
+        .then((data) => {
+          setQuotes(data[0]);
+        });
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getQuote} > Change simpson's quote </button>
+      <QuoteCard 
+      key = {quotes.index}
+      character = {quotes.character}
+      image = {quotes.image}
+      quote = {quotes.quote} 
+/>
+     
     </div>
   );
 }
---
+++ src/components/QuoteCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const QuoteCard = ({ quote, character, image }) => (
+  <figure className="QuoteCard">
+    <img src={image} alt={character} />
+    <figcaption>
+      <blockquote>{quote}</blockquote>
+      <cite>{character}</cite>
+    </figcaption>
+  </figure>
+);
+
+export default QuoteCard;
